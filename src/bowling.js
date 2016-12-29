d7ba405b4e315cfb11984fe3c9c0b00ebcf86d04
Gianluca Ciarcelluti <gianluca@ciarcelluti.it>
2016-12-29 23:19:09 +0100

    Partita con tutti i tiri che colpiscono un birillo.

--- src/bowling.js
+++ src/bowling.js
@@ -1,6 +1,11 @@
 var Bowling = {
     calcolaPunteggio: function(tiri) {
-        // aggiungere il codice
-        return null;
+        var punteggio = 0;
+        
+        tiri.forEach(function(tiro, indice) {
+            punteggio += tiro[0] + tiro[1];
+        });
+        
+        return punteggio;
     }
 };
--- test/bowlingTest.js
+++ test/bowlingTest.js
@@ -1,8 +1,13 @@
 describe("bowling", function() {
 
     it("partita con tutti i tiri a vuoto", function() {
-        var tiri = 0;
-        expect(Bowling.calcolaPunteggio(tiri)).toEqual(null);
+        var tiri = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]];
+        expect(Bowling.calcolaPunteggio(tiri)).toEqual(0);
+    });
+    
+    it("partita con tutti i tiri che colpiscono un birillo", function() {
+        var tiri = [[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]];
+        expect(Bowling.calcolaPunteggio(tiri)).toEqual(20);
     });
 
 });
